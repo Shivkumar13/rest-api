43682f75ba3ce7f55d676399ffaca07198bb9fc0
shivkumar13 <shivkumar.ople97@gmail.com>
2022-02-08 23:58:47 +0530

    Adding /articles path to the API

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	
 )
+
+type Article struct {
+	Title string `json:Title`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+      articles := Articles{
+		  Article{Title:"Test Title", Desc:"Test Description", Content: "Hello World"}
+	  }
+
+
+	fmt.Println("Endpoint Hit: All articles Endpoint")
+	json.NewEncoder(w).Encoder(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "Homepafe Endpoint Hit")
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
 
 }
 
 func handleRequests() {
 
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
